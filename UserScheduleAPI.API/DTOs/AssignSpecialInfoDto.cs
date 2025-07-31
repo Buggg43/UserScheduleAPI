17145a4ae433df9e90815dbe428b3313ee39ee84
Buggg43 <kacperwerra12@gmial.com>
2025-07-31 14:53:42 +0200

    Add User-SpecialInfo relation (GET/POST/DELETE) and validation

---
+++ UserScheduleAPI.API/DTOs/AssignSpecialInfoDto.cs
@@ -0,0 +1,6 @@
+﻿namespace UserScheduleAPI.API.DTOs
+{
+    public class AssignSpecialInfoDto
+    {
+    }
+}
---
+++ UserScheduleAPI.API/Validators/AssignSpecialInfoDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using UserScheduleAPI.API.DTOs;
+
+namespace UserScheduleAPI.API.Validators
+{
+    public class AssignSpecialInfoDtoValidator : AbstractValidator<AssignSpecialInfoDto>
+    {
+        public AssignSpecialInfoDtoValidator() 
+        {
+            RuleFor(x => x.SpecialInfoId)
+                .NotNull()
+                .GreaterThan(0).WithMessage("SpecialInfoId must be greater than 0");
+        }
+    }
+}
