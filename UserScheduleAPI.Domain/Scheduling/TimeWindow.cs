bb9da81a9786d7e1837a0841a9deff130ad6f992
Buggg43 <kacperwerra12@gmial.com>
2025-08-11 22:47:07 +0200

    Fixes :(

---
+++ UserScheduleAPI.Domain/Scheduling/TimeWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserScheduleAPI.Domain.Scheduling
+{
+    public readonly record struct TimeWindow(TimeSpan Start, TimeSpan End)
+    {
+        public bool IsValid => Start < End;
+        public TimeSpan Duration => End - Start;
+        public override string ToString() => $"[{Start:hh\\:mm}-{End:hh\\:mm}]";
+    }
+}
